5093f2437bdc5e45442a2a0f3fd0e982ede73beb
yoshikazuuu <febrianojerry@gmail.com>
2024-09-15 13:08:47 +0700

    feat: about - our events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -192,6 +192,68 @@ export default async function Home() {
             </div>
           </div>
         </section>
+
+        <section
+          id="our-project"
+          className="relative top-0 flex h-fit w-full justify-center bg-black/20 py-[200px]"
+        >
+          <div className="container flex flex-col items-center gap-10 [&>*]:z-10">
+            <h1 className="text-5xl font-bold text-white">
+              Our <Accent>Events</Accent>
+            </h1>
+
+            <div className="grid grid-cols-3 gap-10">
+              {Array.from({ length: 3 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="relative flex flex-col gap-4 rounded-xl bg-white/5 p-6 backdrop-blur-lg"
+                >
+                  <div className="relative">
+                    <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-[#65E0E9] to-[#3E47BE] opacity-75 blur" />
+
+                    <div className="relative">
+                      <Image
+                        src={`/our-project/${i + 1}.png`}
+                        alt="Mission"
+                        width={200}
+                        height={150}
+                        className="aspect-video w-full rounded-lg object-cover"
+                      />
+                    </div>
+                  </div>
+                  <div className="space-y-2">
+                    <div className="flex items-center justify-between">
+                      <h2 className="text-2xl font-bold">Event Name</h2>
+                      <span className="text-lg font-semibold">2021</span>
+                    </div>
+                    <p className="text-justify font-light">
+                      Lorem ipsum dolor sit amet consectetur. At enim mauris
+                      commodo ultricies. Erat sagittis semper dignissim tortor
+                      tellus amet ante habitant.
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className="group relative inline-flex w-[200px]">
+              <Link className="z-20 w-full" href="/contact">
+                <Button className="text-foreground w-full rounded-2xl bg-gradient-to-r from-[#65E0E9] to-[#A269FF] p-6 py-7 text-xl font-bold">
+                  View More
+                </Button>
+              </Link>
+              <div className="animate-tilt absolute inset-0 rounded-xl bg-gradient-to-r from-[#65E0E9] to-[#A269FF] opacity-70 blur-lg transition-all duration-1000 group-hover:-inset-1 group-hover:opacity-100 group-hover:duration-200" />
+            </div>
+
+            <Image
+              src="/our-events/bg.svg"
+              alt="About Us"
+              width={500}
+              height={500}
+              className="absolute top-[-400px] z-0 w-full"
+            />
+          </div>
+        </section>
       </div>
     </>
   );
