a7de9f9caf8935c432686d6cb58b7306b6a85ec7
yoshikazuuu <febrianojerry@gmail.com>
2024-09-16 23:13:16 +0700

    fix: tailwind not genreated upon build in tailwind.config.ts

--- src/components/our-structure.tsx
+++ src/components/our-structure.tsx
@@ -32,6 +32,50 @@ export function OurStructure({ gridItems }: { gridItems: StructureProps[] }) {
       {gridItems.map((item) => (
         <GridItem key={item.id} item={item} />
       ))}
+      {/* <div
+        className={`shadow-custom-shadow relative col-start-3 row-start-1 flex flex-col gap-4 rounded-xl bg-white/5 p-6 backdrop-blur-lg`}
+      >
+        <div className="relative">
+          <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-[#65E0E9] to-[#3E47BE] opacity-75 blur" />
+          <div className="relative">
+            <Image
+              src="/our-structure/pr.png"
+              alt=""
+              width={200}
+              height={150}
+              className="aspect-[2/3] w-full rounded-lg object-cover"
+            />
+          </div>
+        </div>
+        <div className="space-y-2 text-center">
+          <div className="flex flex-col items-center justify-between">
+            <h2 className="text-lg font-bold">1</h2>
+            <span className="font-light"></span>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`shadow-custom-shadow relative col-start-3 row-start-2 flex flex-col gap-4 rounded-xl bg-white/5 p-6 backdrop-blur-lg`}
+      >
+        <div className="relative">
+          <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-[#65E0E9] to-[#3E47BE] opacity-75 blur" />
+          <div className="relative">
+            <Image
+              src="/our-structure/pr-1.png"
+              alt=""
+              width={200}
+              height={150}
+              className="aspect-[2/3] w-full rounded-lg object-cover"
+            />
+          </div>
+        </div>
+        <div className="space-y-2 text-center">
+          <div className="flex flex-col items-center justify-between">
+            <h2 className="text-lg font-bold">2</h2>
+            <span className="font-light"></span>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
